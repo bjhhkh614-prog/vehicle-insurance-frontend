fa34a1bd09cf028454bc36a197367208f1aa694f
bjhhkh614-prog <bjhhkh614@gmail.com>
2026-06-09 00:01:23 +0300

    Initial commit

---
+++ runtime-config.js
@@ -0,0 +1,7 @@
+// Runtime configuration - points to Render backend
+(function () {
+  var cfg = (window.__RUNTIME_CONFIG__ = window.__RUNTIME_CONFIG__ || {});
+  cfg.API_BASE = 'https://vehicle-insurance-backend-a3i7.onrender.com';
+  cfg.SOCKET_URL = 'https://vehicle-insurance-backend-a3i7.onrender.com/';
+  cfg.VITE_API_BASE = 'https://vehicle-insurance-backend-a3i7.onrender.com';
+})();
